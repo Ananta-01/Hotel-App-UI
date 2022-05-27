391890572a838bc86b9e4b398a60c553e9040d8e
Ananta Sontakke <92220773+Ananta-01@users.noreply.github.com>
2022-05-27 16:40:03 +0530

    Add files via upload

---
+++ src/views/screens/HomeScreen.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import {
+  Dimensions,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+  Animated,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import COLORS from '../../consts/colors';
+import hotels from '../../consts/hotels';
+const {width} = Dimensions.get('screen');
+const cardWidth = width / 1.8;
+
+const HomeScreen = ({navigation}) => {
+  const [activeCardIndex, setActiveCardIndex] = React.useState(0);
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+
+  const Card = ({hotel, index}) => {
+    const inputRange = [
+      (index - 1) * cardWidth,
+      index * cardWidth,
+      (index + 1) * cardWidth,
+    ];
+    const opacity = scrollX.interpolate({
+      inputRange,
+      outputRange: [0.7, 0, 0.7],
+    });
+    const scale = scrollX.interpolate({
+      inputRange,
+      outputRange: [0.8, 1, 0.8],
+    });
+    return (
+      <TouchableOpacity
+        disabled={activeCardIndex != index}
+        activeOpacity={1}
+        onPress={() => navigation.navigate('DetailsScreen', hotel)}>
+        <Animated.View style={{...style.card, transform: [{scale}]}}>
+          <Animated.View style={{...style.cardOverLay, opacity}} />
+          <View style={style.priceTag}>
+            <Text
+              style={{color: COLORS.white, fontSize: 20, fontWeight: 'bold'}}>
+              ${hotel.price}
+            </Text>
+          </View>
+          <Image source={hotel.image} style={style.cardImage} />
+          <View style={style.cardDetails}>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <View>
+                <Text style={{fontWeight: 'bold', fontSize: 17}}>
+                  {hotel.name}
+                </Text>
+                <Text style={{color: COLORS.grey, fontSize: 12}}>
+                  {hotel.location}
+                </Text>
+              </View>
+              <Icon name="bookmark-border" size={26} color={COLORS.primary} />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                marginTop: 10,
+              }}>
+              <View style={{flexDirection: 'row'}}>
+                <Icon name="star" size={15} color={COLORS.orange} />
+                <Icon name="star" size={15} color={COLORS.orange} />
+                <Icon name="star" size={15} color={COLORS.orange} />
+                <Icon name="star" size={15} color={COLORS.orange} />
+                <Icon name="star" size={15} color={COLORS.grey} />
+              </View>
+              <Text style={{fontSize: 10, color: COLORS.grey}}>365reviews</Text>
+            </View>
+          </View>
+        </Animated.View>
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View>
+          <Animated.FlatList
+            onMomentumScrollEnd={(e) => {
+              setActiveCardIndex(
+                Math.round(e.nativeEvent.contentOffset.x / cardWidth),
+              );
+            }}
+            onScroll={Animated.event(
+              [{nativeEvent: {contentOffset: {x: scrollX}}}],
+              {useNativeDriver: true},
+            )}
+            horizontal
+            data={hotels}
+            contentContainerStyle={{
+              paddingVertical: 30,
+              paddingLeft: 20,
+              paddingRight: cardWidth / 2 - 40,
+            }}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({item, index}) => <Card hotel={item} index={index} />}
+            snapToInterval={cardWidth}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const style = StyleSheet.create({
+  card: {
+    height: 280,
+    width: cardWidth,
+    elevation: 15,
+    marginRight: 20,
+    borderRadius: 15,
+    backgroundColor: COLORS.white,
+  },
+  cardImage: {
+    height: 200,
+    width: '100%',
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  priceTag: {
+    height: 60,
+    width: 80,
+    backgroundColor: COLORS.primary,
+    position: 'absolute',
+    zIndex: 1,
+    right: 0,
+    borderTopRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardDetails: {
+    height: 100,
+    borderRadius: 15,
+    backgroundColor: COLORS.white,
+    position: 'absolute',
+    bottom: 0,
+    padding: 20,
+    width: '100%',
+  },
+  cardOverLay: {
+    height: 280,
+    backgroundColor: COLORS.white,
+    position: 'absolute',
+    zIndex: 100,
+    width: cardWidth,
+    borderRadius: 15,
+  },
+});
+
+export default HomeScreen;
